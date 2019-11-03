7a2f715e3bd5d6ac64b7025473acd13ffb66267e
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-03 17:53:47 +0100

    Remove package installation

--- build.R
+++ build.R
@@ -1,4 +1,1 @@
-install.packages("knitr")
-install.packages("rmarkdown")
-
 rmarkdown::render("manuscript.Rmd", output_format = rmarkdown::md_document(preserve_yaml = TRUE), output_file = "manuscript.md")
