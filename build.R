68e12c10add1cb9d9bdb0d6bf0adcfb118d37126
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-03 17:48:39 +0100

    Try to install one package per line

--- build.R
+++ build.R
@@ -1,3 +1,4 @@
-install.packages(c("knitr", "rmarkdown"))
+install.packages("knitr")
+install.packages("rmarkdown")
 
 rmarkdown::render("manuscript.Rmd", output_format = rmarkdown::md_document(preserve_yaml = TRUE), output_file = "manuscript.md")
