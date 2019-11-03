ddb96ec6256b28d9f2edc9c1e9bb71845d014e1d
ClemV <22980442+clementviolet@users.noreply.github.com>
2019-11-03 17:42:38 +0100

    Closing a bracket.

--- build.R
+++ build.R
@@ -1,3 +1,3 @@
-install.packages(c("knitr", "rmarkdown")
+install.packages(c("knitr", "rmarkdown"))
 
 rmarkdown::render("manuscript.Rmd", output_format = rmarkdown::md_document(preserve_yaml = TRUE), output_file = "manuscript.md")
