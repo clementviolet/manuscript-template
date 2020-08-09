e64a9fe212401dbbb70ce94cb10b7166864e70d8
ClemV <22980442+clementviolet@users.noreply.github.com>
2020-08-09 22:11:10 +0200

    Add toc to the HTML file :boom:

--- local.compilation.sh
+++ local.compilation.sh
@@ -8,17 +8,29 @@ python .assets/scripts/bibliography.py
 
 # Adding mandatory files
 mkdir -p rendered
+mkdir -p rendered/css
+mkdir -p rendered/js
 cp -r figures rendered/
-cp references.json rendered/
+mv references.json rendered/
 
 # Create tex file
-pandoc manuscript.md -s -o rendered/$repo.tex --filter pandoc-xnos --bibliography=references.json --metadata-file=metadata.json --template=.assets/templates/template.tex
+echo "TeX document"
+pandoc manuscript.md -s -o rendered/$repo.tex --filter pandoc-xnos --bibliography=./rendered/references.json --csl=.assets/templates/ecology-letters.cls --metadata-file=metadata.json --template=.assets/templates/template.tex
+
+# Create html file
+echo "HTML document"
+cp .assets/templates/{style.less,jquery.tocify.css,bootstrap.css} rendered/css/
+cp .assets/templates/{jquery-1.8.3.min.js,jquery-ui-1.9.1.custom.min.js,jquery.tocify.min.js} rendered/js
+pandoc manuscript.md  -o rendered/index.html --filter pandoc-xnos --template=.assets/templates/template.html --bibliography=./rendered/references.json --csl=.assets/templates/ecology-letters.cls --metadata-file=metadata.json --include-in-header=.assets/templates/tocify
+
+# Create docx file
+echo "MS Word document"
+pandoc manuscript.md -s -o rendered/$repo.docx --toc --filter pandoc-xnos --bibliography=./rendered/references.json --csl=.assets/templates/ecology-letters.cls --metadata-file=metadata.json
 
 # Create pdf file
-cd rendered
+echo "PDF Document"
 latexmk $repo.tex -lualatex --file-line-error --interaction=nonstopmode
-
-# Cleaning up
-latexmk -c
+latexmk -c # Cleaning up
 rm -Rf figures references.json
+rm -f references.json
 cd ..
